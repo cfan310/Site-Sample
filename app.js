eb8bb716c585bf674018359491977f6c11e08d1e
cfan310 <cfanning43@gmail.com>
2022-10-14 16:34:31 -0700

    sticky navbar test

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // initial commit
+
+// Sticky Scroll NavBar
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var navBar = document.getElementsByClassName('.navBar');
+var sticky = navBar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
